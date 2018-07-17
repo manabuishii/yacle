9547048da453a4f9de9cd7901dabd2229e8c5435
Manabu ISHII <manabu.ishii.rb@gmail.com>
2018-07-17 17:37:08 +0900

    Remove `go install .`

--- update_badge.sh
+++ update_badge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOTALTESTS=$(go install . ;  cwl/run_test.sh RUNNER=yacle -l | tail -n 2 | head -n 1 | awk '{print $1}' | tr -dc '0123456789')
+TOTALTESTS=$(cwl/run_test.sh RUNNER=yacle -l | tail -n 2 | head -n 1 | awk '{print $1}' | tr -dc '0123456789')
 
 PASSED=$(cwl/run_test.sh RUNNER=yacle 2>&1 | grep "[0-9]* tests passed"  | awk '{ print $1 }')
 
